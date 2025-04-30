d771d6c613888afb198b86f3faca4f8ac762ed8c
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-30 20:54:18 +0200

    Fixed error

--- error.h
+++ error.h
@@ -2,27 +2,15 @@
 #define ERROR_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 /* 
 ERROR return codes:
 -1 - Internal error.
 -2 - Compiling error.
+-3 Fatal error.
 */
 
-void error(int status, char error[1024]) {
-  if (!error || !status) {
-    printf("Internal error, no error or status provided..");
-    exit(-1);
-  }
-
-  switch (status) {
-    case 0: // Warning
-      printf("WARNING: %s", error);
-
-    case 1: // Error
-      printf("ERROR: %s", error)
-      exit(-2);
-  }
-}
+void aurerror(int status, char* error);
 
 #endif
