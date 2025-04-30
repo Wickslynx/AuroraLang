38cd60b5034df98ae2c8ecf2bc0cadfd6ec79292
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-30 20:55:38 +0200

    Fix

--- parse.h
+++ parse.h
@@ -39,46 +39,10 @@ typedef struct {
 
 
 
-Token getToken(const char c) {
-    Token token;
-      
-    if (isdigit(c)) {
-          token.type = TOKEN_INT;
-          token.value = c - '0'; 
-          return token;
-    } else if (isalpha(c)) {
-      token.type = TOKEN_CHAR;
-      token.character = c;
-      return token;
-    } else if (c == '+') {
-          token.type = TOKEN_PLUS;
-          return token;
-    }
-    
-
-    token.type = TOKEN_EOF;
-    return token;
-}
+Token getToken(const char c);
 
 
-int lexer(const char* filename) {
-  char* input;
-  
-  if (!filename) {
-    error(2, "No file given to lexer.");
-  }
-
-  FILE* ifile = fopen(filename, "r");
-  
-  if (ifile == NULL) {
-    error(1, "error opening file: %s", filename);
-  }
-
-  while ((c = fgetc(ifile)) != EOF) {
-    getToken(c);
-  }
-  
-}
+int lexer(const char* filename);
 
 
-#endif
+#endif //PARSE_H
