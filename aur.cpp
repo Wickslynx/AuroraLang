a098aed52fd0eeece7aed8fbbbf262131e9cea18
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-30 20:59:02 +0200

    Did some stuff.

---
+++ aur.cpp
@@ -0,0 +1,13 @@
+// This will be the main file.
+
+#include <stdio.h>
+
+/* Example command: 
+aur hello.aur hello
+*/
+
+int main(int argc, char* argv[]) {
+  char *filename = argv[1]; // First argument is always filename..
+  char *outfile = argv[2]; // Second argument is always out file.
+  return 0;
+}
