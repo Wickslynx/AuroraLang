3b7ad8d0b1da3bcfa43526e369bba5edfa22c2fb
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-01 11:23:21 +0200

    Fix

--- aur.cpp
+++ aur.cpp
@@ -11,8 +11,9 @@ int main(int argc, char* argv[]) {
 
   if (strcmp(argv[1], "--version") == 0) {
     std::cout << "(V) Aur  1.0.0 (BETA) \n  (L) MIT License  \n Build: Stable-release. \n Support: https://github.com/Wickslynx/AuroraLang/issues";
-    return;
+    return -1;
   }
+  
   char *filename = argv[1]; // First argument is always filename..
   char *outfile = argv[2]; // Second argument is always out file.
 
