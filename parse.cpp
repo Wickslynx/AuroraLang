75c32347dc47f63fc919e5871b68bc7bc1dc68b1
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-01 07:41:10 +0200

    Fix

--- parse.cpp
+++ parse.cpp
@@ -2,29 +2,36 @@
 
 Token getToken(const char c) {
     Token token;
-      
-    if (isdigit(c)) {
-          token.type = TOKEN_INT;
-          token.value = c - '0'; 
-          return token;
-    } else if (isalpha(c)) {
-      token.type = TOKEN_CHAR;
-      token.character = c;
-      return token;
-    } else if (c == '+') {
-          token.type = TOKEN_PLUS;
-          return token;
+    switch (c) {
+        case '0' ... '9':
+            token.type=TOKEN_INT;
+            token.value = c - '0';
+            break;
+        case 'a' .... 'z':
+            token.type = TOKEN_CHAR;
+            token.character = c;
+            break;
+        case '+':
+            token.type = TOKEN_PLUS;
+            break;
+        case '-':
+            token.type = TOKEN_MINUS;
+            break;
+        case '/':
+            token.type = TOKEN_DIVIDE;
+            break;
+        
+        default:
+            token.type = TOKEN_EOF;
+            break;
     }
     
 
-    token.type = TOKEN_EOF;
     return token;
 }
 
 
 int lexer(const char* filename) {
-  char* input;
-  
   if (!filename) {
     error(2, "No file given to lexer.");
   }
@@ -34,9 +41,12 @@ int lexer(const char* filename) {
   if (ifile == NULL) {
     error(1, "error opening file...";
   }
-
+    
+  char c;
+    
   while ((c = fgetc(ifile)) != EOF) {
     getToken(c);
   }
-  
+
+  fclose(ifile);
 }
