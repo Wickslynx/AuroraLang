d60eff9a66c27711a166cccb060be0a683efa1ed
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-01 09:31:50 +0200

    Fix

--- aur.cpp
+++ aur.cpp
@@ -1,6 +1,6 @@
 // This will be the main file.
 
-#include <stdio.h>
+#include <iostream>
 
 /* Example command: 
 aur hello.aur hello
@@ -9,5 +9,7 @@ aur hello.aur hello
 int main(int argc, char* argv[]) {
   char *filename = argv[1]; // First argument is always filename..
   char *outfile = argv[2]; // Second argument is always out file.
+
+  std::cout << "Compiling " << filename << " to" << outfile; // Just print a message for now.
   return 0;
 }
