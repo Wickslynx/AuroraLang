0b97d5be0d6564e8a85cb401ca6d048e93d13eef
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-01 11:09:58 +0200

    Fix

--- aur.cpp
+++ aur.cpp
@@ -10,6 +10,6 @@ int main(int argc, char* argv[]) {
   char *filename = argv[1]; // First argument is always filename..
   char *outfile = argv[2]; // Second argument is always out file.
 
-  std::cout << "Compiling " << filename << " to" << outfile; // Just print a message for now.
+  std::cout << "Compiling " << filename << " to " << outfile << "..."; // Just print a message for now.
   return 0;
 }
