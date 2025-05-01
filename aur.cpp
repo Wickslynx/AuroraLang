8cd1b14b3c38593b836b8e19d423a57e6f727732
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-01 11:18:10 +0200

    Fix

--- aur.cpp
+++ aur.cpp
@@ -7,6 +7,11 @@ aur hello.aur hello
 */
 
 int main(int argc, char* argv[]) {
+
+  if (strcmp(argv[1], "version") == 0) {
+    std::cout << "Aur - Stable release \n Version: 1.0.0 (BETA) \n MIT License";
+    return;
+  }
   char *filename = argv[1]; // First argument is always filename..
   char *outfile = argv[2]; // Second argument is always out file.
 
