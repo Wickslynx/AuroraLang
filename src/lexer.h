b47483a627b44c7a3b033491a3fe8c4627a8209f
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-05 19:55:07 +0200

    Fix

--- src/lexer.h
+++ src/lexer.h
@@ -46,7 +46,7 @@ typedef struct {
     union {
       int value;  // For ints.
       char character; // For chars.
-      char* strValue; // For strings.
+      const char* strValue; // For strings.
     };
 } Token;
 
