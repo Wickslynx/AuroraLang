2603efcbbbcecfd9a67bd428acd43a350cb2643f
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-05 17:40:25 +0000

    Moved stuff

---
+++ install.sh
@@ -0,0 +1,37 @@
+echo "Installing AUR..."
+
+while true; do
+    echo "Do you want to install all additional dependencies? (Y/N)"
+    read dependencies
+
+    if [[ "$dependencies" == "Y" ]]; then
+        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+            sudo apt update
+            sudo apt-get install nasm g++
+            sudo apt upgrade g++ # To prevent C++ 20 not supported.
+            break
+        elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+            winget install --id=NASM.NASM -e
+            break
+        else
+            echo "Automatic dependency installation is not supported on your operating system."
+            break
+        fi
+    elif [[ "$dependencies" == "N" ]]; then
+        echo "Continuing without installing dependencies..."
+        break
+    else 
+        echo "Unknown option, try again..."
+    fi
+done
+
+cd src
+
+echo "Do you want to install the program into the system? (Y/N)"
+read install
+
+if [[ "$install" == "Y" ]]; then
+    make install
+elif [[ "$install" == "N" ]]; then
+    make
+fi
