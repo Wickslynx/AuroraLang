d20b0c68a7a40b75fbab0f1b60cb31f1fcc13f7b
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-08 18:23:02 +0200

    Fix

--- src/generator.cpp
+++ src/generator.cpp
@@ -111,7 +111,7 @@ void emit(std::string out) {
     cmd << "nasm -f elf64 temp-aurolang-asm.asm -o temp-aurolang.o && ld temp-aurolang.o -o " << out; 
 
     for (char c : out) {
-        if (isalnum(c) || c == '"' || c == '-') {
+        if (!isalnum(c) || c == '"' || c == '-') {
             std::cout << "Suspicious input detected... Are you sure you want to run this? If you want to override this, please type \"override\" \n Command you are about to run: " << out << "\n:";
             std::cin >> uresponse;
             if (uresponse != "override") {
